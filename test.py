acb25dfdca9aa166c0ff72001eb831cf7039db41
Mosi45-git <139042409+Mosi45-git@users.noreply.github.com>
2025-01-09 19:50:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
-import sys
+import os
 
-# 打印整个 argv 列表
-print("All command line arguments:", sys.argv)
 
-# 打印脚本名称
-print("Script name:", sys.argv)
+USERINFO = os.environ["USERWXMINITOKEN"]
+print(USERINFO)
 
-# 打印传递给脚本的第一个参数（如果存在）
-if len(sys.argv) > 1:
-    print("First argument:", sys.argv)
-else:
-    print("No arguments were provided.")
-
-# 遍历并打印所有参数（除了脚本名称）
-for i in range(1, len(sys.argv)):
-    print(f"Argument {i}:", sys.argv[i])
-  
